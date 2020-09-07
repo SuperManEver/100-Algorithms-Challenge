17c9a380a0b434b6ba91fd36fe27913a32201e92
SuperManEver <nikita.luparev@gmail.com>
2020-09-07 22:36:21 +0500

    arrayMaximalAdjacentDifference

--- src/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ src/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,21 @@
-function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+export function absDiff(xs: number[]): number {
+  return xs.reduce((acc, n) => Math.abs(acc - n), 0)
 }
 
-console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+function arrayMaximalAdjacentDifference(xs: number[]): number {
+  let max = 0
+
+  for (let i = 0; i < xs.length; i++) {
+    const conseq = xs.slice(i, i + 2)
+
+    const temp = absDiff(conseq)
+
+    if (temp > max) {
+      max = temp
+    }
+  }
+
+  return max
+}
+
+export default arrayMaximalAdjacentDifference
